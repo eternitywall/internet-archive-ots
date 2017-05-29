cf17ac28b28ac0da093e36a7e018cdacce5482f8
Luca Vaccaro <luck87@gmail.com>
2017-05-29 17:44:59 +0200

    Add bower-opentimestamps package

--- gulpfile.js
+++ gulpfile.js
@@ -25,32 +25,6 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('app/stylesheets/'));
 });
 
-gulp.task('compress', function() {
-  gulp.src('app/assets/javascripts/bundle.js')
-      .pipe(babili({
-          mangle: {
-              keepClassNames: true
-          }
-      }))
-    .pipe(gulp.dest('assets/javascripts'))
-});
-
-gulp.task('browserify', function() {
-    var options = {
-        continueOnError: false, // default = false, true means don't emit error event
-        pipeStdout: false, // default = false, true means stdout is written to file.contents
-        customTemplatingThing: "test" // content passed to gutil.template()
-    };
-    var reportOptions = {
-        err: true, // default = true, false means don't write err
-        stderr: true, // default = true, false means don't write stderr
-        stdout: true // default = true, false means don't write stdout
-    }
-    return gulp.src('./')
-        .pipe(exec('browserify -r javascript-opentimestamps script.js -o assets/javascripts/bundle.js', options))
-        .pipe(exec.reporter(reportOptions));
-});
-
 gulp.task('javascript', function() {
   return gulp.src('app/assets/javascripts/application/*.js')
     .pipe(addsrc('app/assets/javascripts/vendor/index.js'))
